43dab7f881a6c4f1676086d127f0aac356faf360
amarrical <amarrical@gmail.com>
2014-06-06 17:02:25 -0500

    Adding necessary files.

---
+++ src/RuleBender/RuleParsers/UglyFlatRuleParser.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UglyFlatRuleParser.cs" company="ImprovingEnterprises">
+//     Copyright (c) ImprovingEnterprises. All rights reserved.
+// </copyright>
+// <author>Anthony Marrical</author>
+//-----------------------------------------------------------------------
+namespace RuleBender.RuleParsers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using RuleBender.Entity;
+    using RuleBender.Interface;
+
+    /// <summary>
+    /// Implements IRuleParser with no strategy pattern.
+    /// </summary>
+    public class UglyFlatRuleParser : IRuleParser
+    {
+        #region [ IRuleParser Methods ]
+
+        /// <summary>
+        /// Parses rules to determine which rules should be run.
+        /// Based on those rules, it pulls/creates the mail messages to send out.
+        /// </summary>
+        /// <param name="mailRules">The mail rules to be evaluated.</param>
+        /// <param name="startTime">Time at which the process started.</param>
+        /// <returns>A collection of MailRules which need to be run.</returns>
+        public IList<MailRule> ParseRules(IList<MailRule> mailRules, DateTime startTime)
+        {
+            throw new NotImplementedException();
+        } 
+
+        #endregion
+    }
+}
---
+++ test/RuleBender.Test/MailSenderTests/MailSenderTests.cs
@@ -0,0 +1,67 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MailSenderTests.cs" company="ImprovingEnterprises">
+//     Copyright (c) ImprovingEnterprises. All rights reserved.
+// </copyright>
+// <author>Anthony Marrical</author>
+//-----------------------------------------------------------------------
+namespace RuleBender.Test.MailSenderTests
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    using NUnit.Framework;
+
+    using Rhino.Mocks;
+
+    using RuleBender.Interface;
+    using RuleBender.Logic;
+    using RuleBender.RuleParsers;
+
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Tests are self documenting")]
+    [TestFixture]
+    public class MailSenderTests
+    {
+        #region [ Fields ]
+
+        private MailSender sender;
+
+        private IRuleRepo       ruleRepo;
+        private IEmailService   emailService;
+        private IRuleParser     ruleParser;
+
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+            var ruleParsers = new List<IRuleParser>
+                              {
+                                  new UglyFlatRuleParser(),
+                                  new CommentedFlatRuleParser(),
+                                  new StragetyRuleParser(),
+                                  new EasyTestStragetyRuleParser()
+                              };
+
+            this.ruleRepo       = MockRepository.GenerateStrictMock<IRuleRepo>();
+            this.emailService   = MockRepository.GenerateStrictMock<IEmailService>();
+            this.ruleParser     = ruleParsers[0];
+
+            this.sender = new MailSender(this.ruleRepo, this.emailService, this.ruleParser);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.ruleRepo       .VerifyAllExpectations();
+            this.emailService   .VerifyAllExpectations();
+        }
+
+        #region [ Tests ]
+
+
+
+        #endregion
+    }
+}
